1448ff62ed8299eadbe00339d0c037fec878a809
Reshetov Ivan <re@mail.ru>
2025-07-10 20:39:26 +0300

    start sfml

--- sfml-game/main.cpp
+++ sfml-game/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-void main() {
-	cout << "hello";
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Update the window
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 }
